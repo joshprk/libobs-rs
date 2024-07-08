63cd963d1d1b1230bb7b28d88674937fb117cbbf
Joshua Park <123624726+joshprk@users.noreply.github.com>
2024-07-07 23:06:39 -0400

    refactoring unsafe api and completed string types

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,83 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(improper_ctypes)]
-#![allow(clippy::approx_constant)]
-#![allow(clippy::unreadable_literal)]
-#![allow(rustdoc::bare_urls)]
+use std::{sync::Mutex, thread::{self, ThreadId}};
 
-pub mod wrapper;
+pub use types::ObsPath;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(feature = "unsafe")]
+pub mod libobs;
+#[cfg(not(feature = "unsafe"))]
+mod libobs;
+
+mod types;
+
+static OBS_THREAD_ID: Mutex<Option<ThreadId>> = Mutex::new(None);
+
+pub enum ObsError {
+    Generic,
+    ThreadFailure,
+}
+
+/// Interface to the OBS context. 
+/// 
+/// Only one context can exist across all threads.
+/// Any attempt to create a new context while there 
+/// is an existing one will error.
+#[derive(Debug)]
+pub struct ObsContext {
+    _obs_context_shutdown_zst: _ObsContextShutdownZST
+}
+
+impl ObsContext {
+    pub fn new(info: StartupInfo) -> Result<Self, ObsError> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Debug, Default, Hash, PartialEq, PartialOrd)]
+pub struct StartupInfo {
+    libobs_data_path: ObsPath,
+    plugin_bin_path: ObsPath,
+    plugin_data_path: ObsPath,
+}
+
+/// Internal hack to ensure that OBS frees memory
+/// in the correct order. The library crashes if
+/// you free its members before shutting down
+/// the OBS context.
+#[derive(Debug)]
+struct _ObsContextShutdownZST {}
+
+impl _ObsContextShutdownZST {
+    /// Creates a new `_ObsContextShutdownZST`.
+    /// 
+    /// Creating this struct outside of 
+    /// `ObsContext` is fine because libobs 
+    /// checks if the library was started before
+    /// attempting to shut down, but why would
+    /// you want to?
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Drop for _ObsContextShutdownZST {
+    fn drop(&mut self) {
+        if !is_obs_thread() {
+            return
+        }
+
+        unsafe { libobs::obs_shutdown() }
+    }
+}
+
+/// Checks whether or not the current thread has
+/// created a `ObsContext` and is the OBS thread.
+/// 
+/// If the mutex is poisoned, it will only return
+/// false.
+pub fn is_obs_thread() -> bool {
+    if let Ok(thread_id) = OBS_THREAD_ID.lock() {
+        return *thread_id == Some(thread::current().id())
+    }
+
+    return false
+}
---
+++ src/types.rs
@@ -0,0 +1,108 @@
+use std::env;
+use std::ffi::{c_char, CStr, CString};
+use std::num::{NonZero, NonZeroU8};
+use std::path::{Path, PathBuf};
+
+#[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ObsString {
+    inner: CString,
+}
+
+impl ObsString {
+    pub fn new<T: Into<Vec<u8>>>(t: T) -> Self {
+        let v = t.into()
+            .into_iter()
+            .filter(|c| *c != b'\0')
+            .map(|x| NonZeroU8::new(x).unwrap())
+            .collect::<Vec<NonZero<u8>>>();
+
+        Self { inner: CString::from(v) }
+    }
+
+    pub fn as_ptr(&self) -> *const c_char {
+        self.inner.as_ptr()
+    }
+}
+
+impl From<String> for ObsString {
+    fn from(s: String) -> Self {
+        Self::new(s)
+    }
+}
+
+impl From<&str> for ObsString {
+    fn from(s: &str) -> Self {
+        Self::new(s)
+    }
+}
+
+impl From<CString> for ObsString {
+    fn from(s: CString) -> Self {
+        Self::new(s)
+    }
+}
+
+impl From<&CStr> for ObsString {
+    fn from(s: &CStr) -> Self {
+        Self::new(s.to_owned())
+    }
+}
+
+impl From<Vec<u8>> for ObsString {
+    fn from(v: Vec<u8>) -> Self {
+        Self::new(v)
+    }
+}
+
+impl Into<*const c_char> for ObsString {
+    fn into(self: ObsString) -> *const c_char {
+        self.as_ptr()
+    }
+}
+
+#[derive(Clone, Debug, Default, Hash, PartialEq, PartialOrd)]
+pub struct ObsPath {
+    inner: PathBuf,
+}
+
+impl ObsPath {
+    pub fn new<P: AsRef<Path>>(p: P) -> Self {
+        let mut r = env::current_exe()
+            .unwrap()
+            .to_path_buf();
+
+        r.pop();
+        r.push(p);
+
+        Self { inner: r }
+    }
+
+    pub fn push<P: AsRef<Path>>(&mut self, p: P) {
+        self.inner.push(p)
+    }
+
+    pub fn pop(&mut self) -> bool {
+        self.inner.pop()
+    }
+
+    pub fn into_obs_string(self) -> ObsString {
+        let is_dir = self.inner.is_dir();
+        let mut bytes = self.inner
+            .into_os_string()
+            .into_encoded_bytes()
+            .into_iter()
+            .filter_map(|c| {
+                if c == b'\\' {
+                    return Some(b'/')
+                }
+                Some(c)
+            })
+            .collect::<Vec<u8>>();
+
+        if is_dir {
+            bytes.push(b'/');
+        }
+
+        ObsString::new(bytes)
+    }
+}
