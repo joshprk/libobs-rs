61d17c033426bc4a1e7972c5e49b3112659ca08e
Hendrik Lind <hendrik@sshcrack.me>
2025-11-21 15:03:36 +0100

    fix(test): remove unused imports for cargo-obs-build test project

--- scripts/test_assets/test_cargo_obs_build/build.rs
+++ scripts/test_assets/test_cargo_obs_build/build.rs
@@ -1,7 +1,3 @@
-use std::{env, path::PathBuf};
-
-use cargo_obs_build::{ObsBuildConfig, build_obs_binaries};
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     cargo_obs_build::install().unwrap();
